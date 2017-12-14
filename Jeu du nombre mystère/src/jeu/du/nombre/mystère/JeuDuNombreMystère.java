7568bab333d2b218b0900201d77045c6a60a1957
Unknown <hugo.deleuze@gmail.com>
2017-12-14 22:51:54 +0100

    Ajout du contrôle de saisie
    Ajout d'un contrôle des ebtrées claviers
    Modification d'une boucle saisie mal placée

--- Jeu du nombre mystère/src/jeu/du/nombre/mystère/JeuDuNombreMystère.java
+++ Jeu du nombre mystère/src/jeu/du/nombre/mystère/JeuDuNombreMystère.java
@@ -18,30 +18,59 @@ public class JeuDuNombreMystère {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Initialisation des variables
+    byte nombrMystere = 0; //initialisation de la variable aleatoire
+    byte essai = 0; //initialisation de la réponse du joueur
+    char reponse = 'o'; //initialisation de la condition de boucle
+    
         // Présentation du jeu
      System.out.println("Bonjour,");
      System.out.println("Jouons ensemble au jeu du nombre mystère.");
      System.out.println("Je choisi un nombre compris entre 1 et 100");
      System.out.println("saura-tu le retrouver?");
-     byte nombrMystere = 0; //initialisation de la variable aleatoire
-     byte essai = 0; //initialisation de la réponse du joueur
-     char reponse = 'o'; //initialisation de la condition de boucle
-     Scanner sc = new Scanner(System.in); //
-     do{ nombrMystere = (byte) (Math.random()*100);
-         System.out.println("debug:"+ nombrMystere);
-            do{ System.out.println("Ta réponse?");
-                essai = sc.nextByte();
-                    if(essai > nombrMystere) //Teste si le nombre est plus grand
-                     System.out.println("Nombre trop grand.");
-                    else if (essai < nombrMystere)
+     
+     Scanner sc = new Scanner(System.in); //Objet scanner
+ 
+     do{ // tant que la reponse est "o" nous restons dans la boucle principale
+         nombrMystere = (byte) (Math.random()*100); // generation du nombre mystère
+         
+         System.out.println("debug:" + nombrMystere);// code temporaire pour visualiser le nombre mystere
+            
+         do{ // Tant que le joueur n'a pas trouvé le nombre mystère nous restons dans la boucle
+             System.out.println("Ta réponse?");
+                
+             do{ //contrôle des entrées clavier (compris entre 0 et 100)
+                    essai = 0;
+                    essai = sc.nextByte();
+                    if(essai < 0 && essai > 100)
+                    System.out.println("entrée incorrete! veuillez saisir un nombre compris entre 0 et 100");
+                }while(essai < 0 && essai > 100);
+   
+                sc.nextLine(); //vidage de la ligne lue
+                    
+                if(essai > nombrMystere) //Teste si le nombre est plus grand
+                     System.out.println("Nombre trop grand."); //le switch aurai ici pu être utilisé
+                
+                else if(essai < nombrMystere)
                      System.out.println("Nombre trop petit."); //Teste si le nombre est plus petit
-                    else {                                
-                     System.out.println("Bien joué! On recommence? (o/n)"); // Rejouer?
-                     reponse = sc.nextLine().charAt(0);}
-            }while(essai == nombrMystere); //condition de sortie de la boucle = trouver le nombre               
+                
+                        else       
+                            System.out.println("Bien joué! On recommence? (o/n)"); // Rejouer?                                
+                                
+            }while(essai == nombrMystere); //condition de sortie de la boucle = trouver le nombre  
+        
+            do{ // contrôle entrée clavier (o/n)
+                reponse = 'o';
+                reponse = sc.nextLine().charAt(0);//possibilité de mette ici une boucle de contrôle des entrées clavier (o/n)
+                sc.nextLine(); //vidage de la ligne lue
+                if(reponse != 'o' && reponse != 'n')
+                System.out.println("entrée incorrete! veuillez saisir o ou n");    
+            }while(reponse != 'o' && reponse != 'n');
+                 
         }while(reponse == 'o'); // Condition de sortie du programme
+     
     System.out.println("Merci d'avoir joué avec moi.");
-    System.out.println("Au revoir ...");
+    System.out.println("Au revoir ... ;-)");
+    // Fin du programme
     }
 }
